9dd828c7b4b63f62b6a998682f5508c862a2f2df
maneth wijeratne <wijeratne.maneth@iisgalvanimi.edu.it>
2024-03-07 18:45:09 +0000

    ultimo commit

--- lista-contatti/src/app/lista-contatti/lista-contatti.component.ts
+++ lista-contatti/src/app/lista-contatti/lista-contatti.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-lista-contatti',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './lista-contatti.component.html',
   styleUrl: './lista-contatti.component.css'
 })
@@ -12,13 +13,13 @@ export class ListaContattiComponent {
 
   persone = [
     
-    {nome: "Rafael", cognome: "Leao", eta: "19", numtel: "3289827578", "isOnline": true, colore: "giallo" },
+    {nome: "Rafael", cognome: "Leao", eta: "19", numtel: "3289827578", "isOnline": true, colore: "rgb(255, 255, 0)", img: "assets/img/Leao.jpg"},
  
-    {nome: "Theo", cognome: "Hernandez", eta: "19", numtel: "3512345351", "isOnline": true, colore: "rosso" },
+    {nome: "Theo", cognome: "Hernandez", eta: "19", numtel: "3512345351", "isOnline": true, colore: "rgb(181 , 0 , 0)", img: "assets/img/Theo.jpg" },
  
-    {nome: "Olivier", cognome: "Giroud", eta: "19", numtel: "3285620357", "isOnline": false, colore: "verde" },
+    {nome: "Olivier", cognome: "Giroud", eta: "19", numtel: "3285620357", "isOnline": false, colore: "rgb(0, 255, 0)", img: "assets/img/image.png" },
 
-    {nome: "Mike", cognome: "Magnain", eta: "19", numtel: "3516788205", "isOnline": false, colore: "arancione" },
+    {nome: "Mike", cognome: "Maignan", eta: "19", numtel: "3516788205", "isOnline": false, colore: "rgb(255, 128, 0)", img: "assets/img/Maignan.jpg" },
 
 
  
